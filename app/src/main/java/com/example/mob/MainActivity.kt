dd5e49ef81619ab705f50b568e53b4e2867a63b0
vlad <linpa.techwizard@gmail.com>
2025-01-18 20:03:30 +0700

    lab1

--- app/src/main/java/com/example/mob/MainActivity.kt
+++ app/src/main/java/com/example/mob/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.mob
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +18,24 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val input: EditText = findViewById(R.id.editText);
+        val but: Button = findViewById(R.id.button);
+        but.setOnClickListener() {
+            if (input.text.toString().trim().length > 0) {
+                val message = when (input.text.toString().trim().toInt()) {
+                    1 -> "Понедельник"
+                    2 -> "Вторник"
+                    4 -> "Четверг"
+                    5 -> "Пятница"
+                    6 -> "Суббота"
+                    7 -> "Воскресенье"
+                    else -> "Введите номер дня недели"
+                }
+                Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Введите число", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
